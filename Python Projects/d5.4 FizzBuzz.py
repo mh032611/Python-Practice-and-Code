fc539bb0e23baee63ef35d9967f39671504d690b
MH032611 <mh032611@gmail.com>
2023-08-11 00:00:08 -0500

    Update d5.4 FizzBuzz.py

--- Python Projects/d5.4 FizzBuzz.py
+++ Python Projects/d5.4 FizzBuzz.py
@@ -8,3 +8,13 @@ for number in range(1, 101):
         print("Buzz")
     else:
         print (number)
+
+
+print(["FizzBuzz" if i%3 == 0 and i%5 == 0 else "Buzz" if i%5 == 0 else "Fizz" if i%3 == 0 else i for i in range(1,101)])
+
+
+"""
+The second code block works without using elif by leveraging a conditional expression (also known as a ternary operator). 
+In Python, a conditional expression has the form x if condition else y, where x is the value to be returned if the condition is True, 
+and y is the value to be returned if the condition is False.
+"""
